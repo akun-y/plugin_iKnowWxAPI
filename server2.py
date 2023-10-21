55478018beaec3b07ece4d11ab7984848dbf5384
akun <win11@bzly.home>
2023-10-21 22:22:21 +0800

    feat: 适应linux下的 服务启动

--- server2.py
+++ server2.py
@@ -71,16 +71,19 @@ async def handle_file(request):
 
 #支持通过POST json方式发送请求
 async def handle_send_msg(request):
+    logger.info("handle_send_msg:{}".format(request))
     data =await request.json()
     
     keys = {'user', 'msg', 'to_user_id'}
     if not keys.issubset(data):
         return _resp_error('参数不完整')
-
+    logger.info("_rsa_verify:{}".format(data))
     #验证签名
     if not _rsa_verify(data['msg'], data['sign'], data['user']):
         logger.error('handle_send_msg 签名验证失败')
         return web.HTTPBadRequest(text='数据包验证失败')
+    
+    logger.info("handle_send_msg:{}".format(data))
     
     type = data['type'].upper()
     if type == 'IMAGE':
@@ -92,13 +95,41 @@ async def handle_send_msg(request):
     
     return web.json_response(data)
 
-app = web.Application(client_max_size=1024*1024*20)
 
-app.router.add_get('/', handle)
-app.router.add_get('/wx', handle)
-app.router.add_post('/send', handle_send_msg)
-app.router.add_post('/send/file', handle_file)
 
+
+
+async def handle(request):
+    return web.Response(text="Hello, World!")
+
+async def init():
+    pass
+async def setup():
+    app = web.Application()
+    
+    app.router.add_get('/', handle)
+    
+    app.router.add_get('/wx', handle)
+    app.router.add_post('/send', handle_send_msg)
+    app.router.add_post('/send/file', handle_file)
+
+
+    server_fs = []
+    single = web.AppRunner(app)
+    await single.setup()
+    server_fs.append(web.TCPSite(single, port=9092).start())
+
+
+    double = web.AppRunner(app)
+    await double.setup()
+    server_fs.append(web.TCPSite(double, host="localhost", port=9093).start())
+
+    return asyncio.ensure_future(asyncio.gather(*server_fs))
+def start_aiohttp():
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    srv = loop.run_until_complete(setup())
+    loop.run_forever()
 
 def server_run2(config,channel):
     global handle_message_process,_config
@@ -107,7 +138,21 @@ def server_run2(config,channel):
         
     _config = config
     handle_message_process = MessageProc(channel)
-    logger.info("server_run2:", config['port'])
-    web.run_app(app, host='0.0.0.0', port=config['port'])
-
+    
+    logger.info("server_run2:{}".format(config['port']))
+    
+    print("1")
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(init())
+    print("2")
+    
+    start_aiohttp()
+    time.sleep(25)
+    # while True:
+    #     time.sleep(1.1)
+    
+    # handle_message_process = MessageProc(channel)
+    # logging.info("server_run2:", config['port'])
+    # web.run_app(app, host='0.0.0.0', port=config['port'])
 
